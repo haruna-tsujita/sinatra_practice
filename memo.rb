2a9e5b2bc4937db9f28c9b59bd76ccda9baf8892
haruna <snwxxx29@gmail.com>
2021-07-30 16:16:38 +0900

    作業途中　変更したメモを保存する前まで

--- memo.rb
+++ memo.rb
@@ -16,6 +16,12 @@ def read_memo
   @memos = files.map {|file| JSON.load(File.read(file))}
 end
 
+def show_memo
+  @id = params[:id]
+  read_memo
+  @memo = @memos.find {|x| x["id"].include?(@id)}
+end
+
 get '/memo' do
   read_memo
   erb :memo_top
@@ -32,7 +38,25 @@ get '/new_memo' do
 end
 
 get '/memos/:id' do
+  show_memo
+  erb :show_memo
+end
+
+delete '/memos/:id' do
   @id = params[:id]
+  File.delete("data/memos_#{@id}.json")
+  redirect to ("/memo")
+  erb :show_memo
+end
+
+get '/memos/:id/edit' do
+  show_memo
+  erb :edit_memo
+end
+
+patch '/memos/:id' do
+  show_memo
+  #保存するコードを書く
   erb :show_memo
 end
 
@@ -48,12 +72,10 @@ __END__
   <h1>メモアプリ</h1>
   <ul>
     <% @memos.each do |memo| %>
-      <li><a href="/show_memo<% memo["id"] %>"><%= memo["title"] %></a></li>
+      <li><a href="/memos/<%= memo["id"] %>"><%= memo["title"] %></a></li>
     <% end %>
   </ul>
   <p><a href=/new_memo>追加</a></p>
-  <p><a href=/show_memo>中身を見る</a></p>
-  
 </body>
 </html>
 
@@ -66,9 +88,9 @@ __END__
 </head>
 <body>
   <p>ここに入力してください</p>
-  <form method="post" action="top">
-    <input type="text" name="title" id="title" value="">
-    <textarea name="content" id="memo" value="" rows="3"></textarea>
+  <form method="post" action="memo">
+    <input type="text" name="title" id="title" value=""></br>
+    <textarea name="content" id="memo" value="" rows="3"></textarea></br>
     <input type="submit" value="送信">
   </form>
 </body>
@@ -83,11 +105,15 @@ __END__
 </head>
 <body>
   <h1>メモアプリ</h1>
-  <p>メモの内容</p>
-  <form method="get" action="edit_memo">
+  <h2>タイトル</h2>
+    <p><%= @memo["title"] %></p>
+  <h2>本文</h2>
+    <p><%= @memo["content"] %></p>
+  <form method="get" action="/memos/<%= @memo["id"]%>/edit">
     <input type="submit" value="変更">
   </form>
-  <form method="get" action="top">
+  <form method="post">
+    <input type="hidden" name="_method" value="delete">
     <input type="submit" value="削除">
   </form>
 </body>
@@ -103,9 +129,17 @@ __END__
 <body>
   <h1>メモアプリ</h1>
   <p>メモの内容（編集画面）</p>
-  <form method="get" action="top">
+  <form method="post" action="/memos/<%= @memo["id"]%>">
+  <div>
+    <input type="text" name="title" value="<%= @memo["title"] %>">
+  </div>
+  <div>
+    <textarea name="content" row="5"><%= @memo["content"] %></textarea>
+  </div>
+  <div>
+    <input type="hidden" name="_method" value="patch">
     <input type="submit" value="変更">
+  </div>
   </form>
 </body>
 </html>
-
